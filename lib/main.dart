a505be6f01a1d94e3e85b4c6a0c49e147d3906c8
akshaysonawaneS <akshaysonawane10526@gmail.com>
2020-02-15 12:28:34 +0530

    Initial

---
+++ lib/main.dart
@@ -0,0 +1,85 @@
+
+import 'package:QuoteIt/views/quote_list.dart';
+import 'package:QuoteIt/views/thought_list.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return HomePage();
+  }
+}
+
+
+
+class HomePage extends State<MyApp>{
+  int _currentIndex = 0;
+  bool dark = false;
+  
+  Widget callPage(int currentIndex){
+      switch(currentIndex){
+        case 0:return QuoteList();
+        case 1:return Thoughtlist();
+
+        default:return QuoteList();
+      }
+
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: "Quote",
+        theme: ThemeData(
+          brightness: dark ? Brightness.dark  : Brightness.light,
+          primaryColor: dark ? Colors.grey[900] : Colors.blue,
+          accentColor: dark ? Colors.grey[50] : Colors.blue
+        ),
+        home: Scaffold(
+      appBar: AppBar(
+        title: Center(child: Text("Quote IT")),
+      ),
+      body: callPage(_currentIndex),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.brightness_2),
+        onPressed: (){
+          if(dark == false){
+            dark = true;
+          }
+          else{
+            dark = false;
+          }
+          setState(() {
+          });
+        }
+
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentIndex,
+        onTap: (value){
+          _currentIndex = value;
+          setState(() {
+            
+          });
+        },
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.book),
+            title: Text("Quotes"),
+            backgroundColor: Colors.blue
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.chat_bubble_outline),
+            title: Text("ShowerThoughts"),
+            backgroundColor: Colors.blue
+          )
+        ],
+      ),
+    ),
+    );
+  }
+
+  
+}
---
+++ lib/views/thought_list.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Thoughtlist extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text("Shower"),
+    );
+  }
+}
